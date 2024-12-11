7cfe6123eb3b8feccfa4ae88d10312e4be24ef99
yolaatar <laatarjo@gmail.com>
2024-12-11 10:23:08 +0100

    placeholder

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Jeux from "./pages/Jeux";
 
 const App = () => {
 	return (
@@ -29,6 +30,7 @@ const App = () => {
 						<Route path="/" element={<Home />} />
 						<Route path="/about" element={<About />} />
 						<Route path="/contact" element={<Contact />} />
+            <Route path="/jeux" element={<Jeux />} />
 					</Routes>
 				</main>
 
